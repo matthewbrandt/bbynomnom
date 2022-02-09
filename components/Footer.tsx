d3c871b0de0acbc40d1cb27b78e8af75b7cd03ad
Stephanie Handsteiner <st.handsteiner@gmail.com>
2022-02-09 21:59:06 +0100

    bugfix: alignment of footer in responsive view
    closes #57

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,11 +12,9 @@ export default function Footer({isDetailpage, isFullWidth}: Footer) {
 			<>
 				<footer className={styles.footer}>
 					<div className={styles.footer__pre}>
-						<div className={styles.footer__preLeft}>
-							<p className={styles.footer__preLeftHeader}>Verkaufe dein Essen</p>
-							<p className={styles.footer__preLeftText}>Mit Cook Eat kannst Du die tollen Essen mit lieben Menschen in deiner Nachbarschaft teilen. Das spart dir Kosten und anderen Zeit. Probier es gleich aus!</p>
-							<a href="https://www.cookeat.ch" className="btn btn--orange">Jetzt Anbieten</a>
-						</div>
+						<p className={styles.footer__preHeader}>Verkaufe dein Essen</p>
+						<p className={styles.footer__preText}>Mit Cook Eat kannst Du die tollen Essen mit lieben Menschen in deiner Nachbarschaft teilen. Das spart dir Kosten und anderen Zeit. Probier es gleich aus!</p>
+						<a href="https://www.cookeat.ch" className="btn btn--orange">Jetzt Anbieten</a>
 						<div className={styles.footer__preRight}/>
 					</div>
 					<div className={styles.footer__legal}>
